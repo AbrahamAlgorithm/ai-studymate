cf33a5b676b99d93b0abf5f5c2c982bc42eb8238
AbrahamAlgorithm <abrahamfolorunso6@gmail.com>
2024-08-17 18:09:58 +0100

    Starts with nextjs

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Main>
+      <Section>
+        <Container>
+          <h1>Heading</h1>
+          <p>Content</p>
+        </Container>
+      </Section>
+    </Main>
+  );
+}
