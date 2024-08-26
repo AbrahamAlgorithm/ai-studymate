e1b48365018938d00c45be63bc3e5e7ef7d807e4
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-08-26 21:32:28 +0100

    feat: implementing the main section

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>AI-StudyMate</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Friend</span></p>
+                <p>How can i help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Briefly summarize this concept: urban planning</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Brainstorm team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+                <div className="card">
+                    <p>Improve the readability of the following code</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Main
