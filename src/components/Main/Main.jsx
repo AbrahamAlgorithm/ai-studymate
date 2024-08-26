87e2b6433e9d0e2e779045ce249ea7503ecabf57
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-08-26 22:29:07 +0100

    feat: main section ui done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -32,6 +32,20 @@ const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+
+            <div className="main-bottom">\
+                <div className="search-box">
+                    <input type="text" placeholder="Enter a prompt here" />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                    Built with love by Abraham Algorithm
+                </p>
+            </div>
         </div>
     </div>
   )
