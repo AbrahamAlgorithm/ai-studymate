5dacaa6749621313d5d8703cfe6e2ec3f64b2434
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-08-26 19:25:56 +0100

    feat: setting the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Sidebar/>
+    </>
   )
 }
 
