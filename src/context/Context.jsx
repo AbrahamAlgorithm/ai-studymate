5e42414206b8c27498c14023b3eaa9fad0ef872d
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-08-27 16:47:49 +0100

    feat: setting the media queries

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -14,14 +14,52 @@ const ContextProvider = (props) => {
     const [resultData,setResultData] = useState("");
 
 
+    const delayPara = (index,nextWord) => {
+        setTimeout(function () {
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
+
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
+
     const onSent = async (prompt) => {
 
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        const response = await run(input)
-        setResultData(response)
+        let response;
+        if (prompt !== undefined) {
+            response = await run(prompt);
+            setRecentPrompt(prompt)
+        }
+        else
+        {
+            setPrevPrompts(prev=>[...prev,input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
+        
+        let responseArray = response.split("**");
+        let newResponse="" ;
+        for(let i = 0; i < responseArray.length; i++)
+        {
+            if (i === 0 || i%2 !== 1) {
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += "<b>"+responseArray[i]+"</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        let newResponseArray = newResponse2.split(" ");
+        for(let i=0; i<newResponseArray.length;i++)
+        {
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+" ")
+        }
         setLoading(false)
         setInput("")
     }
@@ -36,7 +74,8 @@ const ContextProvider = (props) => {
         loading,
         resultData,
         input,
-        setInput
+        setInput,
+        newChat
     }
 
     return (
